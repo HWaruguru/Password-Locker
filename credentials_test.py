56a46ff213087c1477eb46a41f5e67a46c0a9913
Waruguru <hannah.waruguru8@gmail.com>
2021-09-06 00:17:45 +0300

    Edit the credential class

---
+++ credentials_test.py
@@ -0,0 +1,29 @@
+import unittest
+from credentials import Credential
+
+class TestCredential(unittest.TestCase):
+    '''
+	Test class that defines test cases for the credentials class behaviours.
+
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+    def setUp(self):
+        '''
+            Set up method to run before each test cases.
+        '''
+        self.new_cred = Credential("instagram", "Hannah", "hanPass1")
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_cred.account_name, "instagram")
+        self.assertEqual(self.new_cred.username, "Hannah")
+        self.assertEqual(self.new_cred.password, "hanPass1")
+
+
+    
+if __name__ == '__main__':
+    unittest.main()
+
